bf5c2ea603c6416ead3fc673b86ed1fe5c362e20
Your Name <macbook@MacBook-Air-de-Macbook.local>
2026-02-02 12:21:48 +0100

    creation de la page d'inscription

---
+++ src/Controller/inscriptionController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class inscriptionController extends AbstractController
+{
+    #[Route('/inscription', name: 'app_inscription')]
+    public function index(): Response
+    {
+        return $this->render('inscription/inscription.html.twig');
+    }
+}
