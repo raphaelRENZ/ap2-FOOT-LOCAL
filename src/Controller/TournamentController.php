a7ec6fbad361b620b3633d2a7b2bf5ac9bac73fe
Your Name <macbook@MacBook-Air-de-Macbook.local>
2026-02-21 16:42:00 +0100

    feat: ajout de l'api lister les tournois

--- src/Controller/TournamentController.php
+++ src/Controller/TournamentController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse; // Obligatoire pour l'API
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TournamentController extends AbstractController
 {
+    // --- ROUTES WEB (HTML pour le navigateur) ---
+
     #[Route('/tournois', name: 'app_tournois', methods: ['GET'])]
     public function index(): Response
     {
-        // TODO: Récupérer la liste des tournois depuis la base de données
         $tournois = [
             ['id' => 1, 'nom' => 'Coupe Locale 2026', 'date_debut' => '2026-02-01'],
             ['id' => 2, 'nom' => 'Championnat Régional', 'date_debut' => '2026-03-15'],
@@ -25,23 +27,37 @@ final class TournamentController extends AbstractController
     #[Route('/tournois/{id}', name: 'app_tournois_detail', methods: ['GET'])]
     public function detail(int $id): Response
     {
-        // TODO: Récupérer le tournoi et ses matchs depuis la base de données
+        $tournoi = ['id' => $id, 'nom' => 'Coupe Locale 2026', 'date_debut' => '2026-02-01'];
+        return $this->render('tournament/detail.html.twig', [
+            'tournoi' => $tournoi,
+            'matchsAVenir' => [],
+            'anciensMatchs' => [],
+        ]);
+    }
+
+    // --- ROUTES API (JSON pour Postman) ---
+
+    #[Route('/api/tournaments', name: 'api_tournaments_index', methods: ['GET'])]
+    public function apiIndex(): JsonResponse
+    {
+        $tournois = [
+            ['id' => 1, 'nom' => 'Coupe Locale 2026', 'date_debut' => '2026-02-01'],
+            ['id' => 2, 'nom' => 'Championnat Régional', 'date_debut' => '2026-03-15'],
+        ];
+
+        return $this->json($tournois); // Retourne les tournois en JSON
+    }
+
+    #[Route('/api/tournaments/{id}', name: 'api_tournaments_detail', methods: ['GET'])]
+    public function apiDetail(int $id): JsonResponse
+    {
         $tournoi = [
             'id' => $id,
             'nom' => 'Coupe Locale 2026',
             'date_debut' => '2026-02-01',
-        ];
-        $matchsAVenir = [
-            ['domicile' => 'AS Montagne', 'exterieur' => 'US Rivière', 'date' => '2026-02-05'],
-        ];
-        $anciensMatchs = [
-            ['domicile' => 'FC Forêt', 'exterieur' => 'SC Plage', 'score' => '1-1', 'date' => '2026-01-20'],
+            'statut' => 'En cours'
         ];
 
-        return $this->render('tournament/detail.html.twig', [
-            'tournoi' => $tournoi,
-            'matchsAVenir' => $matchsAVenir,
-            'anciensMatchs' => $anciensMatchs,
-        ]);
+        return $this->json($tournoi); // Retourne le détail en JSON
     }
 }
