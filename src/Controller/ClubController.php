70493ef7d0e6afeb273144a3d4465942095ac527
Your Name <macbook@MacBook-Air-de-Macbook.local>
2026-02-21 16:34:55 +0100

    feat: separation des routes Web et API pour les clubs

--- src/Controller/ClubController.php
+++ src/Controller/ClubController.php
@@ -4,18 +4,19 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ClubController extends AbstractController
 {
+    // --- PARTIE WEB (HTML pour le navigateur) ---
+
     #[Route('/clubs', name: 'app_clubs', methods: ['GET'])]
     public function index(): Response
     {
-        // TODO: Récupérer la liste des clubs par ville depuis la base de données
         $clubs = [
             ['id' => 1, 'nom' => 'AS Montagne', 'ville' => 'Montville'],
             ['id' => 2, 'nom' => 'US Rivière', 'ville' => 'Rivertown'],
-            ['id' => 3, 'nom' => 'FC Forêt', 'ville' => 'Forestcity'],
         ];
 
         return $this->render('club/index.html.twig', [
@@ -26,22 +27,35 @@ final class ClubController extends AbstractController
     #[Route('/clubs/{id}', name: 'app_clubs_detail', methods: ['GET'])]
     public function detail(int $id): Response
     {
-        // TODO: Récupérer le club et ses joueurs depuis la base de données
+        $club = ['id' => $id, 'nom' => 'AS Montagne', 'ville' => 'Montville'];
+        return $this->render('club/detail.html.twig', [
+            'club' => $club,
+            'joueurs' => []
+        ]);
+    }
+
+    // --- PARTIE API (JSON pour Postman et Expo) ---
+
+    #[Route('/api/clubs', name: 'api_clubs_index', methods: ['GET'])]
+    public function apiIndex(): JsonResponse
+    {
+        $clubs = [
+            ['id' => 1, 'nom' => 'AS Montagne', 'ville' => 'Montville'],
+            ['id' => 2, 'nom' => 'US Rivière', 'ville' => 'Rivertown'],
+        ];
+        return $this->json($clubs);
+    }
+
+    #[Route('/api/clubs/{id}', name: 'api_clubs_detail', methods: ['GET'])]
+    public function apiDetail(int $id): JsonResponse
+    {
         $club = [
             'id' => $id,
             'nom' => 'AS Montagne',
             'ville' => 'Montville',
             'adresse' => '123 Rue du Stade',
-            'telephone' => '01 23 45 67 89',
+            'telephone' => '01 23 45 67 89'
         ];
-        $joueurs = [
-            ['nom' => 'Dupont', 'prenom' => 'Jean', 'poste' => 'Attaquant'],
-            ['nom' => 'Martin', 'prenom' => 'Pierre', 'poste' => 'Défenseur'],
-        ];
-
-        return $this->render('club/detail.html.twig', [
-            'club' => $club,
-            'joueurs' => $joueurs,
-        ]);
+        return $this->json($club);
     }
 }
